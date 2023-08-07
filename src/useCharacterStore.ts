f47345a0f735da280d7d8ce7c7f90e8cb9522128
Andrew Durnford <andrewdurnford3@gmail.com>
2023-08-07 18:28:13 +1000

    fix: Calculate proficiency bonus from level

--- src/useCharacterStore.ts
+++ src/useCharacterStore.ts
@@ -16,6 +16,7 @@ interface CharacterState {
   setName: (name: string) => void
   setRace: (raceId: keyof typeof races) => void
   setClass: (classId: keyof typeof classes, level: number) => void
+  proficiencyBonus: () => number
   abilityScores: () => Record<
     keyof typeof abilities,
     { score: number; modifier: number }
@@ -38,6 +39,13 @@ export const useCharacterStore = create<CharacterState>()((set, get) => ({
   setName: (name) => set(() => ({ name })),
   setRace: (raceId) => set(() => ({ raceId })),
   setClass: (classId, level) => set(() => ({ classId, level })),
+  proficiencyBonus: () => {
+    const level = get().level
+    if (level % 4 === 0) {
+      return Math.floor(level / 4) + 1
+    }
+    return Math.floor(level / 4) + 2
+  },
   abilityScores: () =>
     Object.keys(abilities).reduce(
       (acc, abilityId) => {
@@ -66,9 +74,8 @@ export const useCharacterStore = create<CharacterState>()((set, get) => ({
   abilityChecks: () =>
     Object.entries(skills).reduce(
       (acc, [skillId, { abilityId }]) => {
-        // TODO: move to class
-        const proficiencyBonus = 2
-        // TODO: calculate from class proficiencies
+        const proficiencyBonus = get().proficiencyBonus()
+        // TODO: calculate from class proficiency choices
         const proficient = false
         const abilityModifier = get().abilityScores()[abilityId].modifier
         const modifier = abilityModifier + (proficient ? proficiencyBonus : 0)
@@ -84,8 +91,7 @@ export const useCharacterStore = create<CharacterState>()((set, get) => ({
   savingThrows: () =>
     Object.keys(get().abilityScores()).reduce(
       (acc, abilityId) => {
-        // TODO: move to class
-        const proficiencyBonus = 2
+        const proficiencyBonus = get().proficiencyBonus()
         const proficient = classSavingThrowProficiencies.some(
           (x) => x.classId === get().classId && x.abilityId === abilityId,
         )
